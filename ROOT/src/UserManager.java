bf16768593e5bd894c338ba7852c831607f68434
황규현 <57509844+hgyuhyeon@users.noreply.github.com>
2020-12-01 20:26:09 +0900

    Update UserManager.java

--- ROOT/src/UserManager.java
+++ ROOT/src/UserManager.java
@@ -6,18 +6,22 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserManager {
 	public void makeUser(String userid, String userpw) throws ClassNotFoundException, SQLException, IOException {
+		//유저 생성
+		
 		//유저 디렉토리 생성, index 파일 생성
-		String original = "/var/lib/tomcat8/webapps/ROOT/src/roomlistforuser.jsp";
-		File oriuserinfo = new File(original);
-		String newpath = "/var/lib/tomcat8/webapps/ROOT/user/"+userid;
-		File userinfo = new File(newpath);
+		
+		String path = "/var/lib/tomcat8/webapps/ROOT/user/"+userid;
+		File userinfo = new File(path);
 		userinfo.createNewFile(); //디렉토리 및 파일 생성
 		//파일 복사
+		String original = "/var/lib/tomcat8/webapps/ROOT/doc/roomlistforuser.jsp";
+		File oriuserinfo = new File(original);
 		Files.copy(oriuserinfo.toPath(), userinfo.toPath(), StandardCopyOption.REPLACE_EXISTING);
 		//파일 내용 수정
 		/* 이곳에 원본 파일 sql문 찾아서 mainuser부분 이름을 바꿔야함*/
@@ -32,7 +36,29 @@ public class UserManager {
     	state.close();
     	conn.close();
 	}
+	public boolean comfirmUser(String userid, String userpw) throws ClassNotFoundException, SQLException {
+		//로그인 시 유저 정보 확인
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/testdb", "user", "1234");
+		Statement state = conn.createStatement();
+		
+		String sql = "SELECT * FROM user WHERE id='"+userid+"' AND pw='"+userpw+"';";
+    	ResultSet rs = state.executeQuery(sql);
+    	
+    	state.close();
+    	conn.close();
+    	
+    	if(rs.next()) {
+    		rs.close();
+    		return true;
+    	} else {
+    		rs.close();
+    		return false;
+    	}
+	}
 	public void delUser(String userid) throws ClassNotFoundException, SQLException {
+		//유저 삭제(회원탈퇴 시)
+		
 		String path = "/var/lib/tomcat8/webapps/ROOT/user/"+userid;
 		File folder = new File(path);
 		while(folder.exists()) {
